c397348b5f3a7673068149b974983f708719386a
Joe Kirwin <mail@josephkirwin.com>
2017-08-30 23:07:26 -0700

    create main UI

---
+++ app/src/main/java/org/fedorahosted/freeu2f/MainActivity.java
@@ -0,0 +1,46 @@
+package org.fedorahosted.freeu2f;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MAIN_ACTIVITY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Log.d(TAG, "settings button clicked");
+                return true;
+
+            case R.id.action_about:
+                Log.d(TAG, "about button clicked");
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
